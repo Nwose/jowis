11f00c02ac3874fb896be00b331491cc74ebfd3a
Olubayodejoy <jolubayode@gmail.com>
2025-10-05 19:47:44 +0100

    Add canonical tag to Services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,3 +1,4 @@
+import CanonicalTag from "../components/CanonicalTag";
 import { ContactForm } from "../components/ContactForm";
 import CosmicFooter from "../components/CosmicFooter";
 import ProjectShowcase from "../components/ProjectShowcase";
@@ -6,12 +7,17 @@ import WhyUsSection from "../components/service/WhyUsSection";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-black font-sans">
-      <WhyUsSection />
-      <CosmicHero />
-      <ProjectShowcase />
-      <ContactForm />
-      <CosmicFooter />
-    </main>
+    <>
+      {/* ✅ Canonical tag for Services page */}
+      <CanonicalTag url="https://www.jowistudio.com/services" />
+
+      <main className="relative min-h-screen bg-black font-sans">
+        <WhyUsSection />
+        <CosmicHero />
+        <ProjectShowcase />
+        <ContactForm />
+        <CosmicFooter />
+      </main>
+    </>
   );
 }
